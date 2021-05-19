ae2bc4de8918d907e1c9b3d4e950f31803a2a6f5
Sura Jeon <surajeon@gmail.com>
2021-05-19 01:01:39 +0000

    timeline displays on tweet container

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -5,6 +5,8 @@
  */
 
 $(document).ready(function () {
+  $("#tweeted-since").html(timeago.format(new Date()));
+
   const $tweet = $(`<article class="tweet">Hello world</article>`);
   const data = [
     {
@@ -23,7 +25,8 @@ $(document).ready(function () {
       "user": {
         "name": "Descartes",
         "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd" },
+        "handle": "@rd"
+      },
       "content": {
         "text": "Je pense , donc je suis"
       },
@@ -31,20 +34,38 @@ $(document).ready(function () {
     }
   ]
 
-const renderTweets = function(tweets) {
-  // loops through tweets
-  // calls createTweetElement for each tweet
-  // takes return value and appends it to the tweets container
+  const renderTweets = function (tweets) {
+    // loops through tweets
+    // calls createTweetElement for each tweet
+    // takes return value and appends it to the tweets container
+    let $tweet = $(".tweet").append(tweets);
+  }
 
-}
+  const createTweetElement = function (tweet) {
+    return `
+    <article class="tweet">
+      <header>
+        <img src="/images/profile-hex.png">
+        <span>${tweet.user.handle}</span>
+      </header>
+      <div class="tweeted-text">
+        <p>Hello World!</p>
+      </div>
+      <footer class="tweet-footer">
+        <span id="tweeted-since" class="tweeted-since-posted">10 days ago</span>
+        <ul class="footer-icons">
+          <li><a href="#"><i class="fas fa-flag"></i></a></li>
+          <li><a href="#"><i class="fas fa-retweet"></i></a></li>
+          <li><a href="#"><i class="fas fa-heart"></i></a></li>
+        </ul>
+      </footer>
+    </article>
+    `
+    /* Your code for creating the tweet element */
+    // return $tweet;
+  }
+  
+  renderTweets(data);
 
-const createTweetElement = function(tweet) {
-/* Your code for creating the tweet element */
-  let $tweet = $(".tweet").append(tweet); 
-
-  return $tweet;
-}
-
-renderTweets(data);
 });
 
